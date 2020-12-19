76eb6826e6438d9b99d8fe5f9339600888705849
carlos muñoz <carlos.a.munoz2@outlook.com>
2020-12-19 20:29:15 -0300

    Eslint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/app.scss'
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default HelloWorld;
